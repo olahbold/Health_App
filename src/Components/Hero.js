0d2dd0d987b9164714851e1b9006b9548a866d71
olahbold <olahbohdeh@gmail.com>
2023-03-28 16:38:50 +0000

    Update 3 files

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,11 +1,14 @@
 import "../styles.css";
 import Doctor from "../Components/Images/Doctor.png";
+import  Button  from "react-bootstrap/Button";
 export default function Hero() {
   return (
     <div className="hero">
       <img src={Doctor} alt="" />
       <div className="hero-text">
         <h1> The Health Hub </h1>
+        <h6>Empowering You to Make Healthy Choices</h6>
+         <Button > Create Account </Button>
       </div>
     </div>
   );
